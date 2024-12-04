d883696b8b4f56dab5cb86c8a2adeb7b0f6b6495
Ethan Passard <e.passardplprs.fr>
2024-12-04 09:27:19 +0100

    FEAT (modification)
    ajout de deux nouvelles pages
    modifcation.php
    modification.html
    
    modification tetardtest.html

---
+++ modification.php
@@ -0,0 +1,17 @@
+<?php
+var_dump($_POST);
+$bdd = new PDO('mysql:host=localhost;dbname=tli3;charset=utf8', 'root', '');
+$req = $bdd->prepare('SELECT * FROM inscrit WHERE email = :email AND passe = :passe');
+$req->execute(array(
+    'email' => $_POST['email'],
+    'passe' => $_POST['passe']
+));
+
+$donne = $req->fetch();
+var_dump($donne);
+if ($donne == NULL) {
+    echo "vous n'avez pas de compte! veuillez en crée un ! ";
+}
+else
+    echo "vous êtes connecter ! ";
+?>
